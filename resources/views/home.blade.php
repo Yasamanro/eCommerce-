133e942e5c498454d4879c05641af19b978d710a
Arian Behesht Nejad <abn9271@gmail.com>
2015-12-11 01:02:51 +0330

    Complete home page view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,141 +103,195 @@
                 </div>
             </div>
             <br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <!--<br>-->
-                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                            <!-- Indicators -->
-                            <ol class="carousel-indicators">
-                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#myCarousel" data-slide-to="1"></li>
-                                <li data-target="#myCarousel" data-slide-to="2"></li>
-                            </ol>
+        </div>
+    </div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <!--<br>-->
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
 
-                            <!-- Wrapper for slides -->
-                            <div class="carousel-inner" role="listbox">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
 
-                                <div class="item active">
-                                    <img src="../../1621821c-8365-4c2b-b3de-bfa51faf986b.jpg" alt="Chania" width="460" height="345">
-                                </div>
-
-                                <div class="item">
-                                    <img src="../../027932f7-7741-4eed-909a-670f2e00c593.jpg" alt="Chania" width="460" height="345">
-                                </div>
-
-                                <div class="item">
-                                    <img src="../../6a90e760-c2f5-42ce-8a87-a74e4920c78f.jpg" alt="Flower" width="460" height="345">
-                                </div>
-
-                            </div>
-
-                            <!-- Left and right controls -->
-                            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
+                    <div class="item active">
+                        <img src="1621821c-8365-4c2b-b3de-bfa51faf986b.jpg" alt="Chania" width="460" height="345">
                     </div>
-                </div>
-                <div class="row"><br></div>
-                <div class="row"><br></div>
-                <!--<div class="row"><br></div>-->
-                <div class="row whiteBack">
-                    <h3 class="header">محصولات موجود</h3>
-                    <hr>
 
-                    <br>
-                    <h4 class="header">انواع کتاب</h4>
-                    <br>
-                    @foreach($books as $book)
-                        <div class="col-lg-3 mahsool">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <div class="img">
-                                            <img src="{{$book->imgAddress}}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <p>{{$book->title}}</p>
-                                    </div>
-                                </div>
-                                <div class="row greenBack">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <strong><p>{{$book->price}} &nbsp; تومان</p></strong>
-                                    </div>
-                                </div
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <a href="#"><button class="btn myBtn" >بیشتر</button></a>
-                                    </div>
-                                    <div class="col-lg-10 col-lg-offset-1  basket">
-                                        <a href="#"><i class="fa fa-cart-plus  fa-4x"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                    <div class="row whiteBack">
-                    <br>
-                    <h4 class="header">انواع موبایل</h4>
-                    <br>
-                    @foreach($mobiles as $product)
-                        <div class="col-lg-3 mahsool">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <div class="img">
-                                            <img src="{{$product->imgAddress}}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <p>{{$product->title}}</p>
-                                    </div>
-                                </div>
-                                <div class="row greenBack">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <strong><p>{{$product->price}} &nbsp; تومان</p></strong>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-10 col-lg-offset-1">
-                                        <a href=""><button class="btn myBtn" >بیشتر</button></a>
-                                    </div>
-                                    <div class="col-lg-10 col-lg-offset-1  basket">
-                                        <a href="#"><i class="fa fa-cart-plus  fa-4x"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                    <div class="row"><br></div>
-                    <div class="row"><br></div>
-                    <div class="row"><br></div>
-                </div>
-                <div class="row"><br></div>
-                <div class="row"><br></div>
-                <div class="row"><br></div>
-
-                <div class="row whiteBack">
-                    <div class="col-lg-12">
-                        <br>
-                        <p>کلیه حقوق سایت متعلق به فاندو می باشد.</p>
-                        <br>
+                    <div class="item">
+                        <img src="027932f7-7741-4eed-909a-670f2e00c593.jpg" alt="Chania" width="460" height="345">
                     </div>
+
+                    <div class="item">
+                        <img src="6a90e760-c2f5-42ce-8a87-a74e4920c78f.jpg" alt="Flower" width="460" height="345">
+                    </div>
+
                 </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
     </div>
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <!--<div class="row"><br></div>-->
+    <div class="row whiteBack">
+        <h3 class="header">محصولات موجود</h3>
+        <hr>
+    </div>
+
+    <div class="row whiteBack">
+        <br>
+        <h4 class="header">انواع موبایل</h4>
+        <br>
+        @foreach($mobiles as $product)
+            <div class="col-lg-3 mahsool">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <div class="img">
+                                <img src="{{$product->imgAddress}}">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <p>{{$product->title}}</p>
+                        </div>
+                    </div>
+
+                    <div class="row greenBack">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <strong><p>{{$product->price}} &nbsp; تومان</p></strong>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <a href=""><button class="btn myBtn" >بیشتر</button></a>
+                        </div>
+
+                        <div class="col-lg-10 col-lg-offset-1  basket">
+                            <a href="#"><i class="fa fa-cart-plus  fa-4x"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="row whiteBack">
+        <br>
+        <h4 class="header">انواع کتاب</h4>
+        <br>
+        @foreach($books as $product)
+            <div class="col-lg-3 mahsool">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <div class="img">
+                                <img src="{{$product->imgAddress}}">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <p>{{$product->title}}</p>
+                        </div>
+                    </div>
+
+                    <div class="row greenBack">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <strong><p>{{$product->price}} &nbsp; تومان</p></strong>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <a href=""><button class="btn myBtn" >بیشتر</button></a>
+                        </div>
+
+                        <div class="col-lg-10 col-lg-offset-1  basket">
+                            <a href="#"><i class="fa fa-cart-plus  fa-4x"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="row whiteBack">
+        <br>
+        <h4 class="header">انواع لپتاپ</h4>
+        <br>
+        @foreach($notebooks as $product)
+            <div class="col-lg-3 mahsool">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <div class="img">
+                                <img src="{{$product->imgAddress}}">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <p>{{$product->title}}</p>
+                        </div>
+                    </div>
+
+                    <div class="row greenBack">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <strong><p>{{$product->price}} &nbsp; تومان</p></strong>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <a href=""><button class="btn myBtn" >بیشتر</button></a>
+                        </div>
+
+                        <div class="col-lg-10 col-lg-offset-1  basket">
+                            <a href="#"><i class="fa fa-cart-plus  fa-4x"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <div class="row"><br></div>
+    <div class="row whiteBack">
+        <div class="col-lg-12">
+            <br>
+            <p>کلیه حقوق سایت متعلق به فاندو می باشد.</p>
+            <br>
+        </div>
+    </div>
+</div>
 @endsection
